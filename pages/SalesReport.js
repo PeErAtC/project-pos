d76e116fa8f5f10df87cea9b4ea728cd1cf69955
Pacharaphonj <140188324+Pacharaphonj@users.noreply.github.com>
2024-12-20 15:33:01 +0700

    Update SalesReport.js

--- pages/SalesReport.js
+++ pages/SalesReport.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import BackendSidebar from './components/backendsidebar';
+import BackendSidebar from './components/backendsideber';
 import Swal from 'sweetalert2';
 import {
     FaClipboardList,
