ec556672fa0dd5e8fb4d2913cef817a0b1b1ae0e
PeErAtC <128203020+PeErAtC@users.noreply.github.com>
2025-02-26 13:29:13 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,8 @@
 // pages/_app.js
+import './styles.css'; // นำเข้าฟายล์ CSS Global
+import { useEffect } from 'react';
+import App from 'next/app';
 import './styles.css';  // ใช้ path ที่ถูกต้องตามที่ไฟล์ styles.css อยู่
-
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
